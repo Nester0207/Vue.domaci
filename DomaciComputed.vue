78042bb5d2cf571b510c21bc95070ea38a05ed45
Serhii Nesterchuk <s.nesterchuk27@gmail.com>
2025-01-13 20:59:37 +0100

    DomaciComputed

---
+++ DomaciComputed.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, computed } from "vue"; 
+
+const delka = ref(Math.floor(Math.random() * 100));
+const sirka = ref(Math.floor(Math.random() * 100));
+
+const obsah = computed(() => {
+   let sumaObsahu = delka.value + sirka.value;
+   return sumaObsahu;
+});
+</script>
+
+<template>
+    <p>Delka obdelniku je {{ delka }}, sirka je {{ sirka }}, a obsah je {{ obsah }}</p>
+</template>
