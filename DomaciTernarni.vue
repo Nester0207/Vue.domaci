b4aa4b1e41736a3e8b7e0a299d3a8641334570bb
Serhii Nesterchuk <s.nesterchuk27@gmail.com>
2025-01-13 12:43:14 +0100

    Domaci Ukol Ternarni

---
+++ DomaciTernarni.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from "vue";
+
+const nahodneCislo = ref(Math.random());
+setInterval(() => {
+    nahodneCislo.value = (Math.floor(Math.random() * 50));
+}, 2500);
+</script>
+
+<template>
+    <p>Padlo cislo: {{ nahodneCislo }}</p>
+    <p>Cislo je: {{ (nahodneCislo%2) ? "Liche" : "Sude" }} </p>
+    <p>Cislo je mensi nez 25: {{ (nahodneCislo < 25) ? "Ano" : "Ne" }}</p>
+</template>
