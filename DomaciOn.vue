8a3da90b90c0ec614b8444cbef72c7870f175463
Serhii Nesterchuk <s.nesterchuk27@gmail.com>
2025-01-15 14:56:11 +0100

    DonaciOn

---
+++ DomaciOn.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue"
+
+const prvniKostka = ref();
+const druhaKostka = ref();
+
+function hod() {
+   prvniKostka.value = (Math.ceil(Math.random() * 6));
+   druhaKostka.value = (Math.ceil(Math.random() * 6));
+}
+</script>
+
+<template>
+    <h3>Hod kostkami</h3>
+    <button @click="hod">Hod kostkami</button>
+    <p>Prvno kostka: {{ prvniKostka }}</p>
+    <p>Druha kostka: {{ druhaKostka }}</p>
+    <p>Vysledek: {{ (prvniKostka == druhaKostka) ? "Vyhrali jste" : "Hazejte znovu!" }}</p>
+</template>
