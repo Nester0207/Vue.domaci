ea639dacbbe1f21ca3ecb4eac8a189f2e90f3b9b
Serhii Nesterchuk <s.nesterchuk27@gmail.com>
2025-01-27 18:48:41 +0100

    bind

---
+++ DomaciBind.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+
+const cislo = ref(0);
+
+
+</script>
+
+<template>
+
+    <button @click="cislo++" :disabled="cislo >= 10">{{ cislo }}</button>
+
+</template>
