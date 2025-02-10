729bf4c9a68a98654a2b421ce3eac35e23fcb58c
Serhii Nesterchuk <s.nesterchuk27@gmail.com>
2025-02-10 20:44:40 +0100

    ukol dynamicke kolecko

---
+++ DerektivaBindStyleCSS.vue
@@ -0,0 +1,37 @@
+<script setup>
+import {ref} from "vue"
+
+const objektStyle = ref({
+    borderRadius: "50%",
+    backgroundColor: "magenta",
+    height: "7rem",
+    width: "7rem"
+})
+
+function zmenVelkost(argVyska, argSirka) {
+    objektStyle.value.height = `${argVyska}rem`;
+    objektStyle.value.width = `${argSirka}rem`;
+}
+
+function zmenBarvu() {
+    let cervena = Math.floor(Math.random() * 256);
+    let zelena = Math.floor(Math.random() * 256);
+    let modra = Math.floor(Math.random() * 256);
+    objektStyle.value.backgroundColor = "rgb(" + cervena + "," + zelena + "," + modra + ")";
+}
+
+</script>
+
+<template>
+    <h4>Derektiva v-bibd (style + CSS)</h4>
+    <button @click="zmenVelkost(10,10)">Zmen velkost 1</button>
+    <button @click="zmenVelkost(15,15)">Zmen velkost 2</button>
+    <button @click="zmenVelkost(30,30)">Zmen velkost 3</button>
+    <button @click="zmenBarvu()">Barva</button>
+    <div style="width: 7rem; height: 7rem; background-color: pink;" v-bind:style="objektStyle"></div>
+</template>
+
+<style scoped>
+
+    
+</style>
